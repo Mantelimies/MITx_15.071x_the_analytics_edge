5b9b2d55e6caa55df386fe1c4aaf3d6687f2c75f
Joona Rauhamäki <joona.rauhamaki@gmail.com>
2016-04-30 03:14:03 +0200

    add first part of week 2 homework
    add the climate change tasks

--- W2/Unit2_WineRegression.R
+++ W2/Unit2_WineRegression.R
@@ -73,3 +73,11 @@ SSE = sum((wineTest$Price - predictTest)^2)
 SST = sum((wineTest$Price - mean(wine$Price))^2)
 1 - SSE/SST
 
+# Quick Question 
+#In your R console, create a corresponding rank vector by typing
+
+teamRank = c(1,2,3,3,4,4,4,4,5,5)
+wins2012 = c(94,88,95,88,93,94,98,97,93,94)
+wins2013 = c(97,97,92,93,92,96,94,96,92,90)
+cor(teamRank, wins2012)
+cor(teamRank, wins2013)
